766062d2dbaa8986273db72cab4af22e791fec97
rohelk <rohelk@ttu.ee>
2019-02-06 14:49:55 +0200

    Katse

---
+++ src/main/java/esimene/EsimeneApplication.java
@@ -0,0 +1,14 @@
+package esimene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsimeneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsimeneApplication.class, args);
+	}
+
+}
+
